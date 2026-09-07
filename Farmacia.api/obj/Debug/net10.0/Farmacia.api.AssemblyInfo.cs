09bee938dacf2d6ed3cc26aebb3c3245dd02f291
Sal-DS <matheussalda2108@gmail.com>
2026-09-07 15:50:22 -0300

    sla que porra e essa

--- Farmacia.api/obj/Debug/net10.0/Farmacia.api.AssemblyInfo.cs
+++ Farmacia.api/obj/Debug/net10.0/Farmacia.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmacia.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f52ebfb82f27ab02f20938fce85203b2ca39d49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c895635ce63286c66abbd8727f163f08d045da4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmacia.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmacia.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
