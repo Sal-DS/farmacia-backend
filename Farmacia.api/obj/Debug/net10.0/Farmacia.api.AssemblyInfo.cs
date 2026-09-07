2cd8f76273ba90c7992347af4c2789c6f8e50b89
Sal-DS <matheussalda2108@gmail.com>
2026-09-07 15:53:57 -0300

    sla que prr e essa

--- Farmacia.api/obj/Debug/net10.0/Farmacia.api.AssemblyInfo.cs
+++ Farmacia.api/obj/Debug/net10.0/Farmacia.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmacia.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a6c5195c7ea77327ae70f03d3fb1a590b2f81f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26ad14c537d6fa53c0ec8e348203eb98d652ed8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmacia.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmacia.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
